860e2a38a72ac378469ec9a7f9658ddaa14f86de
boocai <onezhjp@gmail.com>
2024-07-10 22:09:05 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'boocai tent',
+    template: '%s | boocai tent',
   },
-  description: 'This is my portfolio.',
+  description: 'This is boocai\'s tent.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'boocai tent',
+    description: 'This is boocai\' tent.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'boocai tent',
     locale: 'en_US',
     type: 'website',
   },
@@ -52,7 +52,7 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-4xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
