cff153a83be7a93bed0de60e0c5aadd0661adb02
pen <heyppen@gmail.com>
2025-01-25 17:56:32 +0800

    add minidate

---
+++ app/minidate/page.tsx
@@ -0,0 +1,21 @@
+export default function Page() {
+  return (
+    <div className="py-16">
+      <div className="mt-8 mx-2">
+        <h1 className="font-bold text-[36px] mb-4">MiniDate</h1>
+        <h2 className="text-2xl" id="privacy">
+          Privacy
+        </h2>
+        <p>
+          MiniDate does not collect any information except UI interactions or
+          share any data with anyone. It also works well without network.
+        </p>
+
+        <h2 className="mt-4 text-2xl" id="support">
+          Support
+        </h2>
+        <p>Any feedback or support, please contact hi@tentt.dev</p>
+      </div>
+    </div>
+  );
+}
